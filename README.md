bc38c60e3b77bbb9111dbc711f640c4ff0ec585e
erick <erickchang1000@fmail.com>
2026-04-20 19:50:44 -0500

    Metodo burbuja

---
+++ README.md
@@ -0,0 +1,12 @@
+# Universidad Politecnica Salesiana
+
+## Estructura de datos
+## Estudiante: Erick chang
+
+## Practica 1.1 Metodo Burbuja
+### Fecha: 20/04/2026
+### Descripcion:    
+
+Cree la clase "Metodo burbuja" que implementa metodo de ordenamiento ascendente,
+descendente y un metodo para imprimir. Con constructor que instancia una variable
+global
---
+++ src/MetodoBurbuja.java
@@ -0,0 +1,42 @@
+public class MetodoBurbuja {
+
+    int[] arreglo;
+
+    // Constructor
+    public MetodoBurbuja(int[]arreglo){
+        System.out.println("Se creo la clase Metodo Burbuja" );
+
+        //1era FORMA
+        //Instanciar con valor creado en constructor
+        this.arreglo =new int[] {10,5,8,0,7};
+
+        //2da FORMA
+        ////Instanciar con valor desde parametro
+        this.arreglo  = arreglo;
+    }
+
+    public void ordenarAcendente(){
+        for(int i =0; i<arreglo.length;i++){
+            for(int j=i+1 ; j<arreglo.length;j++){
+                 if(arreglo[i] > arreglo[j]){
+                    int aux = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j]= aux;
+                 }
+            }
+        }
+    }
+    public void imprimirArreglo(){
+        for (int num : arreglo) {
+            System.out.println(num + ",");
+            
+        }
+        System.out.println();
+    }
+
+    public void ordenarDescendente() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ordenarDescendente'");
+    }
+    
+}
